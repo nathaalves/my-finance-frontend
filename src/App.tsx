26d97c09590c7e91636d1c507717a5d82e842dee
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-04 17:55:31 -0300

    feat: create App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './assets/styles/GlobalStyles';
+import * as theme from './assets/styles/themes/';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 export function App() {
-  return <h1>Olá, mundo!</h1>;
+  return (
+    <ThemeProvider theme={theme['purple']}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
