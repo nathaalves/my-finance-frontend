0ae1d4a477fe332763c0a1c50f63024e61410e0d
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-04 18:46:01 -0300

    feat: create login route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './assets/styles/GlobalStyles';
 import * as theme from './assets/styles/themes/';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
 
 export function App() {
   return (
     <ThemeProvider theme={theme['purple']}>
       <GlobalStyles />
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
