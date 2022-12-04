53b086fb47dd58ce61b710581692925c84805d37
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-04 17:56:44 -0300

    feat: create react root

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './assets/styles/GlobalStyles';
+import * as theme from './assets/styles/themes/';
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme['purple']}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
