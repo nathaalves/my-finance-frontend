74239eccb393ce35da5ae0432453b6fdcfb9a5fa
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-04 18:26:56 -0300

    feat: create Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import styled, { DefaultTheme } from 'styled-components';
+
+type LogoProps = {
+  fontSize?: keyof DefaultTheme['text']['size'];
+};
+
+export const Logo = styled.h2<LogoProps>`
+  font-family: ${({ theme }) => theme.text.fontFamily.logo};
+  font-size: ${({ fontSize, theme }) =>
+    fontSize ? theme.text.size[fontSize] : theme.text.size['6xl']};
+  color: ${({ theme }) => theme.color.primary};
+`;
