aeb4d4410b8864025f3b7a591b0cfabef822fe8b
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-05 19:01:24 -0300

    style: chage font weighgt and add line height

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -10,7 +10,9 @@ export const Link = styled(RouterLink)<LinkProps>`
   font-family: ${({ theme }) => theme.text.fontFamily.base};
   font-size: ${({ fontSize, theme }) =>
     fontSize ? theme.text.size[fontSize] : theme.text.size.base};
-  font-weight: 700;
+  line-height: ${({ fontSize, theme }) =>
+    fontSize ? theme.text.lineHeight[fontSize] : theme.text.lineHeight.base};
+  font-weight: 500;
   color: ${({ theme }) => theme.color.primary};
   cursor: pointer;
 `;
