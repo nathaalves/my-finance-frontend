3c7c97ac47a9e3ac4a522779366e973d0d4763c2
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-12 13:04:35 -0300

    feat: create routes for 'entradas' e 'saidas'

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Registration } from './pages/Registration';
 import { AuthProvider } from './contexts/auth/AuthProvider';
+import { Home } from './pages/Home';
+import { ContentProvider } from './contexts/content/ContentProvider';
+import { Statistics } from './pages/Home/Statistics';
 
 export function App() {
   return (
@@ -12,7 +15,14 @@ export function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AuthProvider />}></Route>
+          <Route path="/" element={<AuthProvider />}>
+            <Route path="/" element={<ContentProvider />}>
+              <Route path="/" element={<Home />}>
+                <Route path="/entradas" element={<Statistics />} />
+                <Route path="/saidas" element={<Statistics />} />
+              </Route>
+            </Route>
+          </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/registration" element={<Registration />} />
         </Routes>
