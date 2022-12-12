7e97770899728fc06b9ac9d713e2f16cac00fbdb
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-12 13:02:29 -0300

    feat: create NavBar component

---
+++ src/pages/Home/NavBar.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { NavButton } from '../../components/NavButton';
+
+export function NavBar() {
+  return (
+    <Container>
+      <NavButton path="/entradas">Entradas</NavButton>
+      <NavButton path="/saidas">Saídas</NavButton>
+    </Container>
+  );
+}
+
+const Container = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  margin-top: 28px;
+`;
