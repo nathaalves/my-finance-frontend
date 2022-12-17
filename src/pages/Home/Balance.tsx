e893be8459c3644b804b87288bc69430244bec94
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-17 13:30:01 -0300

    refactor: uses useContentQuery

--- src/pages/Home/Balance.tsx
+++ src/pages/Home/Balance.tsx
@@ -1,15 +1,10 @@
-import { useContext } from 'react';
 import styled from 'styled-components';
-import { DynamicIcon } from '../../components/DynamicIcon';
-import { Title } from '../../components/Title';
-import {
-  ContentContext,
-  ContentContextType,
-} from '../../contexts/content/ContentContext';
+import { DynamicIcon, Title } from '../../components';
+import { useContentQuery } from '../../hooks/queries/useContentQuery';
 import { formatMoney } from '../../utils/formatMoney';
 
 export function Balance() {
-  const { content } = useContext(ContentContext) as ContentContextType;
+  const { content } = useContentQuery();
 
   const balance = (content?.inflow.total || 0) - (content?.outflow.total || 0);
 
