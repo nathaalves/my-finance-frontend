09f07bb265b4c650cdca9259d09047041a5434b1
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-16 22:01:22 -0300

    feat: create custom hook to request login

---
+++ src/hooks/queries/useRegistration.ts
@@ -0,0 +1,20 @@
+import { AxiosError } from 'axios';
+import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import { requestRegistration } from '../../services/api';
+import type { SignUpBody, SignUpResponseError } from '../../types';
+
+export function useRegistration(data: SignUpBody) {
+  const navigate = useNavigate();
+
+  const { isLoading, isError, error, mutate } = useMutation<
+    void,
+    AxiosError<SignUpResponseError>
+  >(() => requestRegistration(data), {
+    onSuccess: (auth) => {
+      navigate('/login');
+    },
+  });
+
+  return { isLoading, isError, mutate, error };
+}
