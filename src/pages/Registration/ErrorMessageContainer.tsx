c57945c460930dc22c766cde717ed601cc657bb0
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-16 22:16:01 -0300

    feat: create ErrorMessageContainer component

---
+++ src/pages/Registration/ErrorMessageContainer.tsx
@@ -0,0 +1,30 @@
+import { AxiosError } from 'axios';
+import styled from 'styled-components';
+import { ErrorMessage } from '../../components';
+import type { SignUpResponseError } from '../../types';
+
+type ErrorContainerProps = {
+  isError: boolean;
+  error: AxiosError<SignUpResponseError> | null;
+};
+
+export function ErrorMessageContainer({ isError, error }: ErrorContainerProps) {
+  return (
+    <Container>
+      {isError &&
+        error?.response?.data?.details?.map((details) => (
+          <ErrorMessage>{details}</ErrorMessage>
+        ))}
+      {error?.response?.status === 409 && (
+        <ErrorMessage>Usuário já cadastrado.</ErrorMessage>
+      )}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 8px;
+`;
