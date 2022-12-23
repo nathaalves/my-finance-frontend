577c84165f90b660fb3550d27b68888fbbf2aebb
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-23 09:36:32 -0300

    feat: create servicec to delete transaction

--- src/services/api/transactionApi.ts
+++ src/services/api/transactionApi.ts
@@ -4,3 +4,7 @@ import { instance } from './instance';
 export async function addTransaction(body: TransactionBody): Promise<void> {
   await instance.post('/transaction/create', body);
 }
+
+export async function deleteTransaction(id: string): Promise<void> {
+  await instance.delete(`/transaction/delete/${id}`);
+}
