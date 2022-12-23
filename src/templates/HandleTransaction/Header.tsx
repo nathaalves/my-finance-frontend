acd2f6d763a1ec5d721f3062e2a838110765c3dd
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-23 14:30:33 -0300

    style: add box shadow

--- src/templates/HandleTransaction/Header.tsx
+++ src/templates/HandleTransaction/Header.tsx
@@ -31,6 +31,7 @@ const Container = styled.div`
     ${({ theme }) => theme.color.secondary},
     ${({ theme }) => theme.color.primary}
   );
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
 const BackArrow = styled(ImArrowLeft2)`
