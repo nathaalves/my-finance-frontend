268861d336974c6cf5dcf608438eaedd48430f2c
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-01-06 15:45:30 -0300

    feat: create LoadingPage component

---
+++ src/components/LoadingPage.tsx
@@ -0,0 +1,44 @@
+import styled, { keyframes } from 'styled-components';
+
+export function LoadingPage() {
+  return (
+    <Container>
+      <Logo>MyFinance</Logo>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.color.background};
+`;
+
+const Logo = styled.h2`
+  background-image: linear-gradient(
+    45deg,
+    ${({ theme }) => theme.color.primary},
+    ${({ theme }) => theme.color.secondary}
+  );
+  background-size: 300%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  font-family: ${({ theme }) => theme.text.fontFamily.logo};
+  font-size: 5rem;
+  color: transparent;
+
+  animation: gradient 1s infinite alternate;
+
+  @keyframes gradient {
+    0% {
+      background-position: left;
+    }
+    100% {
+      background-position: right;
+    }
+  }
+`;
